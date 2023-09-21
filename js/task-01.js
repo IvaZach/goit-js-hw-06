f6c492e87a7ae35d07923886f4793bb59180d7ef
IvaZach <ivazacherniuk@gmail.com>
2023-09-21 12:59:36 +0300

    Excercise 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,16 @@
-// 1. Порахує і виведе в консоль кількість категорій в ul#categories, 
+// 1. Порахує і виведе в консоль кількість категорій в ul#categories,
 // тобто елементів li.item.
 
-const liEl = document.querySelectorAll('li.item');
+const liEls = document.querySelectorAll('li.item');
+console.log(`Number of categories: ${liEls.length}`);
 
-console.log(`Кількість категорій в ul#categories  -  ${liEl.length}`);
-
-// 2. Для кожного элемента li.item у списку ul#categories, 
-// знайде і виведе в консоль текст заголовку елемента (тегу <h2>) 
+// 2. Для кожного элемента li.item у списку ul#categories,
+// знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
 // і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
+const categoryEl = liEls.forEach(liEl => {
+  const titleEl = liEl.querySelector('h2').textContent;
+  const listEL = liEl.querySelectorAll('ul >li');
+  console.log(`\n`);
+  console.log(`Category: ${titleEl} \n Elements: ${listEL.length}`);
+});
